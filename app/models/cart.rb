e30d96f3dccdb46dd532518c402f8925ceb66db6
ZhengJieGan <ganzj45@gmail.com>
2023-03-01 02:07:38 +0800

    change cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,8 @@ class Cart
   include Mongoid::Document
   include Mongoid::Timestamps
   field :item_id, type: String
+  field :title, type: String
+  field :image, type: String
   field :quantity, type: Integer
   belongs_to :user
 
